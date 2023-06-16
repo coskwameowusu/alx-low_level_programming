4839bfa120aa249cba859ad828ed232c251e72b6
Collins Owusu <coskwameowusu@gmail.com>
2023-06-16 15:30:53 -0700

    ff

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,19 +13,17 @@ int i, j;
 {
 	if (i > j)
 {
-	putchar((i / 10) + 48);
-	putchar((i % 10) + 48);
+	putchar((i / 10) + '0');
+	putchar((i % 10) + '0');
 	putchar(' ');
-	putchar((j / 10) +  48);
-	putchar((j % 10) + 48);
-	if (i != 98 || j != 99)
+	putchar((j / 10) +  '0');
+	putchar((j % 10) + '0');
+	if (i != 98 || j != '0');
 {
 	putchar(',');
 	putchar(' ');
 }
 }
-}
-putchar('\n');
 }
 return (0);
 }
