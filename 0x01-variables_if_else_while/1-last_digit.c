7cce43595efb1a4d31f9ab8db39657ab976fcdac
Collins Owusu <coskwameowusu@gmail.com>
2023-06-17 00:37:18 -0700

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,11 +14,11 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if ((n % 10) > 5)
 {
-	printf("last digit of %d and is greater than 5\n", n, n % 10);
+	printf("last digit of %d is %d  and is greater than 5\n", n, n % 10);
 }
 else if ((n % 10) < 6 && (n % 10) != 0)
 {
-	printf("last digit of %d and is less than 6 not 0\n", n, n % 10);
+	printf("last digit of %d is %d  and is less than 6 not 0\n", n, n % 10);
 }
 else
 {
