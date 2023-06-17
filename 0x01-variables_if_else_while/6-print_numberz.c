a8d0d3897eda4a85468a117d75dd3d1843b020ea
Collins Owusu <coskwameowusu@gmail.com>
2023-06-17 03:18:00 -0700

    dgd

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,11 +6,11 @@
 
 int main(void)
 {
-        int i;
-        for (i=48, i<58; i++)
-        {
-                putchar(i);
-        }
-        putchar('\n');
+	int i;
+
+	for (i = 0; i < 10; i++)
+	putchar((i % 10) + '0');
+
+	putchar('\n');
 	return (0);
 }
