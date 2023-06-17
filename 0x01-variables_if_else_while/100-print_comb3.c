8b88a07f2754fc987270baab7097afdb15c62259
Collins Owusu <coskwameowusu@gmail.com>
2023-06-17 11:50:14 -0700

    e

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,26 +6,24 @@
 
 int main(void)
 {
-        int n,m;
-        for(n=48 n<=56; n++)
-        {
-                for(m=49 m<=57; m++)
-		{
-			if(m > n)
-			{
-				putchar(n);
-				putchar(m);
-				if(n!=58 || m!=57)
-				{
-					 putchar(',');
-                                	 putchar('');
-				}
-			}
-		}
-	}
+	int n, m;
 
-        
-        putchar('\n');
+	for (n = 48; n <= 56; n++)
+{
+	for (m = 49; m <= 57 ; m++)
+{
+	if (m > n)
+{
+	putchar(n);
+	putchar(m);
+	if (n != 58 || m != 57)
+{
+	putchar(',');
+	putchar(' ');
+}
+}
+}
+}
+	putchar('\n');
 	return (0);
 }
-
