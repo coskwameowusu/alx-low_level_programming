95ad2dcfcf8ff49f0a78e165aa946db5d8ff6994
Collins Owusu <coskwameowusu@gmail.com>
2023-06-18 07:19:45 -0700

    d

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,8 +14,8 @@ int i, j;
 	if (i > j)
 {
 	putchar((i / 10) + 48);
-	putchar((i % 10) + 48);
-	putchar(' ');
+	putchar((i % 10) + );
+
 	putchar((j / 10) +  48);
 	putchar((j % 10) + 48);
 	if (i != 98 || j != 99)
