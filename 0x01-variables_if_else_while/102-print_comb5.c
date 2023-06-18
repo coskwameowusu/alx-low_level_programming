e9518f4fcdf77c5c747bcb692d7ed568e1a6672e
Collins Owusu <coskwameowusu@gmail.com>
2023-06-18 12:37:15 -0700

    d

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,31 +1,30 @@
 #include <stdio.h>
 /**
-* main - prints all posible combinations oof 2 digits numbers
+* main - print numbers from 0 t0 9
 * Return:0 (succes)
 */
-
 int main(void)
 {
-int i, j;
-	for (i = 0; i < 100; i++)
-{
-	for (j = 0; j < 100; j++)
-{
-	if (i > j)
-{
-	putchar((i / 10) + 48);
-	putchar((i % 10) + 48);
+	int i;
+	int j;
 
-	putchar((j / 10) +  48);
-	putchar((j % 10) + 48);
-	if (i != 98 || j != 99)
+	for (i = 0; i <= 99; i++)
 {
-	putchar(',');
+	for (j = i; j <= 99; j++)
+	{
+		int tens_i = i / 10;
+		int ones_i = i % 10;
+		int tens_j = j / 10;
+		int ones_j = j % 10;
+
+	putchar('0' + tens_i);
+	putchar('0' + ones_i);
+		putchar(' ');
+	putchar('0' + tens_j);
+	putchar('0' + ones_j);
+		putchar(',');
 	putchar(' ');
+	}
 }
-}
-}
-putchar('\n');
-}
-return (0);
+	return (0);
 }
