812590ed80d31b0d0590496af3fe5adbe7d93331
Collins Owusu <coskwameowusu@gmail.com>
2023-06-21 12:37:22 -0700

    10th

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add -  adds two integers and returns the result.
+ * @b: int to check
+ * Return: every minute of the day
+ */
+
+int add(int a, int b)
+{
+int c;
+c = a + b;
+return (c);
+}
