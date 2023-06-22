8e0c022d66055f9883b54f11b924cc2518b0abeb
Collins Owusu <coskwameowusu@gmail.com>
2023-06-21 17:30:45 -0700

    12

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,23 +1,39 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * main - list all naural numbers below 1024
- * that are multiples of 3 or 5.
- * Return: 0
+ * print_times_table - print the n times table from 0
+ * @n: int to check
+ * Return: n table.
  */
 
-
-int main(void)
+void print_times_table(int n)
 {
+int num, mult, prod;
 
-int i;
-int sum = 0;
-
-	for (i = 0; i < 1024; i++)
-	{
-	if ((i % 3 == 0) || (i % 5 == 0))
-		sum += i;
-	}
-	printf("%d\n", sum);
-	return (0);
+if (n >= 0 && n <= 15)
+{
+for (num = 0; num <= n; num++)
+{
+_putchar('0');
+for (mult = 1; mult <= n; mult++)
+{
+_putchar(',');
+_putchar(' ');
+prod = num * mult;
+if (prod <= 99)
+_putchar(' ')
+if (prod <= 9)
+_putchar(' ')
+if (prod >= 100)
+{
+_putchar((prod / 100) + '0');
+_putchar(((prod / 10)) % 10 + '0');
+}
+else if (prod <= 99 && prod >= 10)
+{
+_putchar((prod / 10) + '0');
+_putchar((prod % 10) + '0');
+}
+_putchar('\n');
+}
+}
 }
