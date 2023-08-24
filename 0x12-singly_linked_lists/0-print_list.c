6e6d3f0ad1aba97a6e91ae835edadd06232cdaf6
Collins Owusu <coskwameowusu@gmail.com>
2023-08-24 13:14:07 -0700

    print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_list - print all
+ * @str: string - (malloc'ed string)
+ * @h: pointer to list_t list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+size_t i = 0;
+
+while (h)
+{
+	if (h -> str == NULL)
+	{
+		printf("[0] (nil)\n");
+	}
+	else 
+	{
+		printf("[%u] %s\n", h -> len, h -> str);
+	}
+	h = h -> next;
+	i++;
+}
+return (i);
+}
